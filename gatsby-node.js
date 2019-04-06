538f762aad0ef22f762d43a63eb45a38040d2621
Grant Glidewell <ggcadc@gmail.com>
2019-04-06 14:43:28 -0700

    init

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+'use strict';
+const Zesty = require('zestyio-api-wrapper');
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  // await getToken with user credentials.
+  // options for getting content, models, media --no version support (only published?)
+  //TODO: create zesty instance
+  // fetch data from endpoints
+  // write data by node type/endpoint
+};
