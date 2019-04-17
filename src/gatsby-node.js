37657010e3b690fbb58fb1db5f9a48f59b800bd2
Grant Glidewell <ggcadc@gmail.com>
2019-04-16 20:10:34 -0700

    added: handle user specified models/items

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -5,10 +5,19 @@ exports.sourceNodes = async (
   configOptions
 ) => {
   const { createNode } = actions;
-  const handleGenerateNodes = (node, name) => {
+  const { userName, password, instanceZUID } = configOptions;
+
+  if (!userName || !password || !instanceZUID) {
+    console.error(
+      'Username, Password, and instance ZUID are required for Zesty.io source plugin'
+    );
+    return;
+  }
+
+  const handleGenerateNodes = (node, name, ZUID) => {
     return {
       ...node,
-      id: createNodeId(node.ZUID),
+      id: createNodeId(ZUID),
       parent: null,
       children: [],
       internal: {
@@ -22,34 +31,55 @@ exports.sourceNodes = async (
   // authenticate session
   async function authedSDK() {
     const auth = new SDK.Auth();
-    const session = await auth.login(
-      configOptions.userName,
-      configOptions.password
-    );
-    return new SDK(configOptions.instanceZUID, session.token);
+    const session = await auth.login(userName, password);
+    return new SDK(instanceZUID, session.token);
   }
 
   const zesty = await authedSDK();
 
-  // options for getting content, models, media --no version support (only published?)
-  if (!configOptions.models && !configOptions.items) {
-    // by default all models and items are fetched
+  const { models, items } = configOptions;
+  // by default all models and items are fetched
+  if (!models && !items) {
     zesty.instance.getModels().then(modelRes => {
-      const models = modelRes.data;
-      models.map(model => {
+      const contentModels = modelRes.data;
+      contentModels.map(model => {
         return zesty.instance.getItems(model.ZUID).then(itemRes => {
-          const items = itemRes.data;
-          items.map(item => {
-            return createNode(handleGenerateNodes(item, model.label)); // TODO: label?
+          const contentModelItems = itemRes.data;
+          contentModelItems.map(item => {
+            return createNode(
+              handleGenerateNodes(item, model.label, item.meta.ZUID)
+            );
           });
         });
       });
     });
   } else {
-    // the user has specified models//items to fetch
-    const endpointsToFetch = [...configOptions.models, ...configOptions.items];
-    // fetch only the specific endpoints and generate nodes
+    // the user has specified models/items to fetch
+    if (models) {
+      models.map(model => {
+        return zesty.instance.getItems(model.ZUID).then(itemRes => {
+          const userModelItems = itemRes.data;
+          userModelItems.map(item => {
+            return createNode(
+              handleGenerateNodes(item, model.label, item.meta.ZUID)
+            );
+          });
+        });
+      });
+    }
+    if (items) {
+      items.map(item => {
+        return zesty.instance.getItem(item).then(res => {
+          const userItem = res.data;
+          return createNode(
+            handleGenerateNodes(
+              userItem,
+              userItem.web.metaTitle,
+              userItem.meta.ZUID
+            )
+          );
+        });
+      });
+    }
   }
-  // fetch data from endpoints
-  // write data by node type/endpoint
 };
